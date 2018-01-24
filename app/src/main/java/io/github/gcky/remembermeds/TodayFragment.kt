89981f5ef3b0696dd6db1a9f80619cb9e9602e52
gcky <gcky.cheng@gmail.com>
2018-01-24 18:54:47 +0000

    Implement basic list view

--- app/src/main/java/io/github/gcky/remembermeds/TodayFragment.kt
+++ app/src/main/java/io/github/gcky/remembermeds/TodayFragment.kt
@@ -1,19 +1,55 @@
 package io.github.gcky.remembermeds
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_main.view.*
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 /**
  * Created by Gordon on 23-Jan-18.
  */
 
 class TodayFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater?.inflate(R.layout.today_fragment, container, false)
+        val view = inflater?.inflate(R.layout.today_fragment, container, false)
+
+        val todayListView: ListView? = view?.findViewById(R.id.today_list_view)
+        todayListView?.adapter = MyCustomAdapter(activity)
+
         return view
+    }
+
+    private class MyCustomAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "ROWWWW"
+            return textView
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
+
     }
 }
